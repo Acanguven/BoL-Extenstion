70b2f9a404167f4685e8212b08f88d19ada45e18
The Law <ahmetcanguven44@gmail.com>
2015-04-14 04:43:50 +0300

    fixed decode

--- front.js
+++ front.js
@@ -2,7 +2,7 @@ var array = [];
 var links = document.getElementsByTagName("a");
 for(var i=0; i<links.length; i++) {
 	if ((links[i].href.substr((~-links[i].href.lastIndexOf(".") >>> 0) + 2)).toLowerCase().indexOf("lua") == 0){
-		links[i].insertAdjacentHTML('beforebegin','<h3 dllink='+links[i].href+' style="background-color:beige;color:brown;fontSize:30px;cursor:pointer;">Bot of Legends Helper: Download Script('+links[i].href.replace(/^.*[\\\/]/, '')+')</h3>');
+		links[i].insertAdjacentHTML('beforebegin','<h3 dllink='+links[i].href+' style="background-color:beige;color:brown;fontSize:30px;cursor:pointer;">Bot of Legends Helper: Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</h3>');
 		links[i].style.display = 'none';
 		array.push(links[i]);
 	}
