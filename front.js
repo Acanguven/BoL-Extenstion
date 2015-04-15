c75e60672331c4d06cd05d866adcfb6f7e6412d4
The Law <ahmetcanguven44@gmail.com>
2015-04-15 15:55:40 +0300

    Added gif

--- front.js
+++ front.js
@@ -24,7 +24,7 @@ var links = document.getElementsByTagName("a");
 for(var i=0; i<links.length; i++) {
 	for(var x = 0; x < testCases.length; x++){
 		if(links[i].href.match(testCases[x].test)){
-			links[i].insertAdjacentHTML('beforebegin','<h3 dltype="'+testCases[x].name+'" dllink='+links[i].href+' style="background-color:beige;color:brown;fontSize:30px;cursor:pointer;"><img src="http://www.getawayspas.com/arrow_animate.gif">&nbsp;&nbsp;&nbsp;Bot of Legends Helper: Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</h3>');
+			links[i].insertAdjacentHTML('beforebegin','<h3 dltype="'+testCases[x].name+'" dllink='+links[i].href+' style="background-color:beige;color:brown;fontSize:30px;cursor:pointer;"><img src="https://raw.githubusercontent.com/thelaw44/BoL-Extenstion/master/arrow_animate.gif">&nbsp;&nbsp;&nbsp;Bot of Legends Helper: Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</h3>');
 			links[i].style.display = 'none';
 			array.push(links[i]);
 		}
