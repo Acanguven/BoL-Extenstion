806ac4011b9b3a0b27c74d5ec27893721214841c
The Law <ahmetcanguven44@gmail.com>
2015-04-15 15:53:59 +0300

    added pastebinRaw pastebinLink githubLink

--- background.js
+++ background.js
@@ -1,7 +1,31 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	chrome.downloads.download({
-	  url: message.url,
-	  filename: message.url.replace(/^.*[\\\/]/, '')
-	});
+	try{
+		if (message.type == "gitHubRaw") {
+			chrome.downloads.download({
+			  url: message.url,
+			  filename: decodeURI(message.url.replace(/^.*[\\\/]/, ''))
+			});
+		};
+		if (message.type == "pastebinRaw") {
+			chrome.downloads.download({
+			  url: message.url,
+			  filename: message.url.split("i=")[1]+".lua"
+			});
+		};
+		if (message.type == "gitHubLink") {
+			var urlLink = "https://raw.githubusercontent.com/" + message.url.replace("/blob", "").split("github.com/")[1];
+			chrome.downloads.download({
+			  url: urlLink,
+			  filename: decodeURI(urlLink.replace(/^.*[\\\/]/, ''))
+			});
+		};
+		if (message.type == "pastebinLink") {
+			var test = /pastebin\.com\/(.*)/
+			var urlLink = "http://pastebin.com/raw.php?i=" + message.url.match(test)[1];
+			chrome.downloads.download({
+			  url: urlLink,
+			  filename: urlLink.split("i=")[1]+".lua"
+			});
+		};
+	}catch(err){alert("Bot of Legends Helper Error:" + error)}
 });
-
--- front.js
+++ front.js
@@ -1,11 +1,34 @@
+var testCases = [
+	{
+		name:"pastebinRaw",
+		test:/pastebin\.com\/raw.php\?i=(.*)/
+	},
+	{
+		name:"pastebinLink",
+		test:/pastebin\.com\/((?!raw).*)/
+	},
+	{
+		name:"gitHubLink",
+		test:/github\.com\/(.*)\.lua/
+	},
+	{
+		name:"gitHubRaw",
+		test:/githubusercontent\.com\/(.*)\.lua/
+	}
+];
+
+
 var array = [];
 var links = document.getElementsByTagName("a");
+
 for(var i=0; i<links.length; i++) {
-	if ((links[i].href.substr((~-links[i].href.lastIndexOf(".") >>> 0) + 2)).toLowerCase().indexOf("lua") == 0){
-		links[i].insertAdjacentHTML('beforebegin','<h3 dllink='+links[i].href+' style="background-color:beige;color:brown;fontSize:30px;cursor:pointer;">Bot of Legends Helper: Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</h3>');
-		links[i].style.display = 'none';
-		array.push(links[i]);
-	}
+	for(var x = 0; x < testCases.length; x++){
+		if(links[i].href.match(testCases[x].test)){
+			links[i].insertAdjacentHTML('beforebegin','<h3 dltype="'+testCases[x].name+'" dllink='+links[i].href+' style="background-color:beige;color:brown;fontSize:30px;cursor:pointer;"><img src="http://www.getawayspas.com/arrow_animate.gif">&nbsp;&nbsp;&nbsp;Bot of Legends Helper: Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</h3>');
+			links[i].style.display = 'none';
+			array.push(links[i]);
+		}
+	}	
 }
 
 for(var x = 0; x < array.length; x++){
@@ -15,6 +38,6 @@ for(var x = 0; x < array.length; x++){
 	});
 }
 
-function dl(obj){
-	chrome.runtime.sendMessage({url:obj.getAttribute("dllink")});
+function dl(obj,type){
+	chrome.runtime.sendMessage({url:obj.getAttribute("dllink"),type:obj.getAttribute("dltype")});
 }
