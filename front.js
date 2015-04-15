ffde47955a28f5523c2afbe86674fcda75cf1c25
The Law <ahmetcanguven44@gmail.com>
2015-04-15 18:12:03 +0300

    open button

--- front.js
+++ front.js
@@ -24,17 +24,23 @@ var links = document.getElementsByTagName("a");
 for(var i=0; i<links.length; i++) {
 	for(var x = 0; x < testCases.length; x++){
 		if(links[i].href.match(testCases[x].test)){
-			links[i].insertAdjacentHTML('beforebegin','<div class="dlhelper" dltype="'+testCases[x].name+'" dllink="'+links[i].href+'"><img src="https://raw.githubusercontent.com/thelaw44/BoL-Extenstion/master/arrow_animate.gif">&nbsp;&nbsp;&nbsp;Bot of Legends Helper: <span>Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</span></div>');
+			links[i].insertAdjacentHTML('beforebegin','<div class="dlhelper" dltype="'+testCases[x].name+'" dllink="'+links[i].href+'"><img src="https://raw.githubusercontent.com/thelaw44/BoL-Extenstion/master/arrow_animate.gif">&nbsp;&nbsp;&nbsp;Bot of Legends Helper: <span>Download Script('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</span> Or <span>Open Link ('+decodeURI(links[i].href.replace(/^.*[\\\/]/, ''))+')</span></div>');
 			links[i].style.display = 'none';
 			array.push(links[i]);
 		}
 	}	
 }
 
+
+
 for(var x = 0; x < array.length; x++){
 	var h3Obj = array[x].previousElementSibling
-	h3Obj.addEventListener("click", function(event){
-		dl(this)
+	h3Obj.childNodes[2].addEventListener("click", function(event){
+		dl(this.parentNode)
+	});
+	h3Obj.childNodes[4].addEventListener("click", function(event){
+		var win = window.open(this.parentNode.getAttribute("dllink"), '_blank');
+  		win.focus();
 	});
 }
 
